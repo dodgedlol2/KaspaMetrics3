fa80798ae769b5c524e8c8458c25651ab1269ff7
dodgedlol2 <dodgedlol2@gmail.com>
2025-06-15 00:59:20 +0200

    Update 2_⛏️_Mining_Difficulty.py

--- pages/2_⛏️_Mining_Difficulty.py
+++ pages/2_⛏️_Mining_Difficulty.py
@@ -329,7 +329,7 @@ header {visibility: hidden;}
 st.markdown('<div class="chart-controls">', unsafe_allow_html=True)
 
 # Create the layout with proper spacing
-col1, col2, col3, spacer, col4 = st.columns([0.7, 0.7, 0.7, 4, 1.4])
+col1, col2, col3, spacer, col4 = st.columns([0.8, 0.8, 0.8, 4, 1.4])
 
 with col1:
     st.markdown('<div class="control-group"><div class="control-label">Hashrate Scale</div>', unsafe_allow_html=True)
